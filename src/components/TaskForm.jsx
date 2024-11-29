ab16bb1b6c4b85cf720edb0fa34454b885a2d85f
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-29 20:55:01 +0300

    added taskform

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,14 @@
+
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const TaskForm = () => {
+
+};
+
+TaskForm.propTypes = {
+  taskToEdit: PropTypes.object,
+  clearEdit: PropTypes.func,
+};
+
+export default TaskForm;
