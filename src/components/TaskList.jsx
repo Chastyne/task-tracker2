ce19dc7ee9b20d1c4e4a28e3b9e9c2012026928e
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-29 21:10:00 +0300

    tasklist functionality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import TaskForm from "./TaskForm";
 
 const TaskList = () => {
+  const tasks = useSelector((state) => state.tasks);
+  const [taskToEdit, setTaskToEdit] = useState(null);
 
   return (
     <div>
-      <TaskForm/>
+      <TaskForm 
+        taskToEdit={taskToEdit}
+        clearEdit={() => setTaskToEdit(null)}
+      />
     </div>
   );
 };
