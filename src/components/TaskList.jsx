741755cd5a8e14661adc42f88ec792406da6a5fa
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-29 21:03:12 +0300

    create tasklist

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import TaskForm from "./TaskForm";
+
+const TaskList = () => {
+
+  return (
+    <div>
+      <TaskForm/>
+    </div>
+  );
+};
+
+export default TaskList;
