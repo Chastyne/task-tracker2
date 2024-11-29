1bddf428933841d82bb89b4f55a0f03a829bc6d8
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-29 21:00:49 +0300

    add taskform to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import TaskForm from './components/TaskForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Task Tracker</h1>
+      <TaskForm/>
     </div>
   );
 }
