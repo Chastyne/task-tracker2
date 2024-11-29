7284e454ac26ff07700e0deb64b1f6e914e81537
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-29 20:44:13 +0300

    update reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,12 +4,14 @@ const initialState = {
   tasks: [],
 };
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
     case ADD_TASK:
       return { ...state, tasks: [...state.tasks, action.payload] };
 
 
     default:
       return state;
+    }
 
-}
+};
 export default reducer;
