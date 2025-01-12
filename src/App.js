73b5a9d9885478bda265815ffe9b58be000cd440
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2025-01-12 12:47:49 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 
 const App = () => (
   <Router>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Task Manager</h1>
+    <div className="nav-links">
+      <Link to="/">Home</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/login">Login</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
